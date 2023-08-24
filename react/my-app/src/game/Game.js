77a76c93dd8fa8227b01ac366c69f15419083f51
Kamil Richert <krichert@atlassian.com>
2023-08-24 15:11:43 +0200

    CHange state in Game

--- react/my-app/src/game/Game.js
+++ react/my-app/src/game/Game.js
@@ -1,10 +1,27 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Score = styled.span`
+    color: ${props => props.isRed ? 'red' : 'black'}
+`;
 
 export const Game = ({ name }) => {
     const [points, setPoints] = useState(0);
 
+    const increase = () => {
+        setPoints(points + 5);
+    }
+
+    const decrease = () => {
+        setPoints(points - 5);
+    }
+
     return <>
         <h1>Witaj w grzę {name}!</h1>
-        <h2>Twoja liczba punktów to: {points}!</h2>
+        <h2>Twoja liczba punktów to: <Score isRed={points < 0}>{points}</Score>!</h2>
+        <div>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+        </div>
     </>
 }
