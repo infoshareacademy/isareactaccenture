01fff8bb0df2670f126f7d81363cd9203b05c5c7
Kamil Richert <krichert@atlassian.com>
2023-08-24 12:12:09 +0200

    Destructing part 3

--- react/my-app/src/Contact.js
+++ react/my-app/src/Contact.js
@@ -1,16 +1,14 @@
 import './Contact.css';
 
-export function Contact(props) {
-    const { data } = props;
-
+export function Contact({ data: { phone, address, email } }) {
     return (
         <div className="contact-container">
             <h3>TEL</h3>
-            <p>{data.phone}</p>
+            <p>{phone}</p>
             <h3>ADRES</h3>
-            <p>{data.address.street} {data.address.number}, {data.address.city}</p>
+            <p>{address.street} {address.number}, {address.city}</p>
             <h3>E-MAIL</h3>
-            <p>{data.email}</p>
+            <p>{email}</p>
         </div>
     )
 }
