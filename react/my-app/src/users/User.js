4acd267981b6ac430505cebac9b40df687628811
Kamil Richert <krichert@atlassian.com>
2023-08-25 12:59:21 +0200

    Added link in user componranrt to navigate to user details

--- react/my-app/src/users/User.js
+++ react/my-app/src/users/User.js
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom';
+
 export const User = ({ user }) => {
-    return <li>{user.name} works in
+    return <li>
+        <Link to={`/users/${user.id}`}>{user.name}</Link>
+        works in
         <span title={user.company.catchPhrase}>{user.company.name}</span>
     </li>
 }
