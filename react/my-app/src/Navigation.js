06d4beb6271fad822d22009cd479a31655972aaf
Kamil Richert <krichert@atlassian.com>
2023-08-25 11:59:09 +0200

    Updated nav to not use pivot and added react router links

--- react/my-app/src/Navigation.js
+++ react/my-app/src/Navigation.js
@@ -1,14 +1,31 @@
-import { Pivot, PivotItem } from '@fluentui/react';
+import { PrimaryButton } from '@fluentui/react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
+const NavContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 15px;
+`
 
 export const Navigation = () => {
     return (
-        <Pivot>
-            <PivotItem headerText="Home" />
-            <PivotItem headerText="About me" />
-            <PivotItem headerText="Forms" />
-            <PivotItem headerText="Games" />
-            <PivotItem headerText="Users" />
-        </Pivot>
+        <NavContainer>
+            <Link to="/">
+                <PrimaryButton text="Home" />
+            </Link>
+            <Link to="/about-me">
+                <PrimaryButton text="About me" />
+            </Link>
+            <Link to="/games">
+                <PrimaryButton text="Games" />
+            </Link>
+            <Link to="/forms">
+                <PrimaryButton text="Forms" />
+            </Link>
+            <Link to="/users">
+                <PrimaryButton text="Users" />
+            </Link>
+        </NavContainer>
     )
 }
