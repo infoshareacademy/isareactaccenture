1c68dcf93462e467905bfd052383e87116d1d2cb
Kamil Richert <krichert@atlassian.com>
2023-08-25 12:56:40 +0200

    Added fetching data in user details

--- react/my-app/src/users/UserDetails.js
+++ react/my-app/src/users/UserDetails.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Text } from '@fluentui/react';
 import { useParams } from 'react-router-dom';
+import { Spinner, SpinnerSize } from '@fluentui/react';
 
 export const UserDetails = () => {
+    const [user, setUser] = useState(null);
     const params = useParams();
 
-    return <Text variant="large">Witaj uzytkowniku o id: {params.id}</Text>
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+            .then(r => r.json())
+            .then(data => {
+                setUser(data);
+            })
+    }, [params.id]);
+
+    if (!user) {
+        return <Spinner size={SpinnerSize.large} />
+    }
+
+    return <Text variant="large">Witaj {user.name}!</Text>
 }
