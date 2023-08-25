8441ee9d07c810edaaf41561890ba8eeb51c1447
Kamil Richert <krichert@atlassian.com>
2023-08-25 14:14:54 +0200

    Improved user details loading state

--- react/my-app/src/users/UserDetails.js
+++ react/my-app/src/users/UserDetails.js
@@ -5,18 +5,35 @@ import { Spinner, SpinnerSize } from '@fluentui/react';
 
 export const UserDetails = () => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     const params = useParams();
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-            .then(r => r.json())
+            .then(r => {
+                if (!r.ok) {
+                    throw new Error('An error occurred');
+                }
+                return r.json()
+            })
             .then(data => {
                 setUser(data);
             })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, [params.id]);
 
-    if (!user) {
+    if (isLoading) {
         return <Spinner size={SpinnerSize.large} />
+    }
+
+    if (error) {
+        return <Text variant="large">Something went wrong!</Text>
     }
 
     return <Text variant="large">Witaj {user.name}!</Text>
