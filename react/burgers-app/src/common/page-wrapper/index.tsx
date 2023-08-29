84bafa25fdf5a232594783d3bb6c83d99854cb60
Kamil Richert <krichert@atlassian.com>
2023-08-29 12:39:45 +0200

    Page wrapper with typing ver 1

---
+++ react/burgers-app/src/common/page-wrapper/index.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode, FC } from 'react';
+import styled from 'styled-components';
+import { Text } from '@fluentui/react';
+
+const PageWrapperContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 15px;
+`;
+
+type Props = { 
+    children: ReactNode, 
+    title: string 
+}
+
+export const PageWrapper: FC<Props> = ({ children, title }) => (
+    <PageWrapperContainer>
+        <Text variant="xxLarge">{title}</Text>
+        {children}
+    </PageWrapperContainer>
+)
