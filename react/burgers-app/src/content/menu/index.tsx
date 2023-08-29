6b144582ecc3d2113f03a1298e81609b6691d98f
Kamil Richert <krichert@atlassian.com>
2023-08-29 13:21:22 +0200

    Added fetching burgers in Menu

--- react/burgers-app/src/content/menu/index.tsx
+++ react/burgers-app/src/content/menu/index.tsx
@@ -1,21 +1,24 @@
-import { DetailsList, SelectionMode } from "@fluentui/react"
+import { useState, useEffect } from 'react'
+import { DetailsList, SelectionMode  } from "@fluentui/react"
 import { PageWrapper } from "../../common/page-wrapper"
 
 const columns = [
-    { key: 'name', name: 'Name', fieldName: 'name', minWidth: 100 },
-    { key: 'ingredients', name: 'Ingredients', fieldName: 'ingredients', minWidth: 100 },
+    { key: 'name', name: 'Name', fieldName: 'name', minWidth: 200 },
+    { key: 'ingredients', name: 'Ingredients', fieldName: 'ingredients', minWidth: 200 },
     { key: 'price', name: 'Price', fieldName: 'price', minWidth: 100 },
 ];
 
 export const Menu = () => {
-    const burgers = [
-        {
-            key: 'id_1',
-            name: 'burger name',
-            ingredients: 'ingredients',
-            price: 30
-        }
-    ]
+    const [burgers, setBurgers] = useState<any>([]);
+
+    useEffect(() => {
+        fetch('https://rest-api-b6410.firebaseio.com/burgers.json')
+            .then(r => r.json())
+            .then(data => {
+                const formattedData = Object.keys(data).map(key => ({ ...data[key], id: key }));
+                setBurgers(formattedData);
+            })
+    }, []);
 
     return (
         <PageWrapper title="Menu">
@@ -23,6 +26,7 @@ export const Menu = () => {
                 columns={columns}
                 items={burgers}
                 selectionMode={SelectionMode.none}
+                viewport={{ width: 500, height: 1000 }}
             />
         </PageWrapper>
     )
