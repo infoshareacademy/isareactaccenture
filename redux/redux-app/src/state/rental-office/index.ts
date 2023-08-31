8123ab3d89e206dd22e2e7cb8bb730a66e9ecd73
Kamil Richert <krichert@atlassian.com>
2023-08-31 13:01:05 +0200

    Finished rental office reducer

--- redux/redux-app/src/state/rental-office/index.ts
+++ redux/redux-app/src/state/rental-office/index.ts
@@ -14,14 +14,40 @@ const initialState: Product[] = [
 ]
 
 // ACTIONS
+const ADD = 'ADD';
+const REMOVE = 'REMOVE';
+const RENT = 'RENT';
+const RETURN = 'RETURN';
+
+type AddAction = { type: typeof ADD, payload: Product }
+type RemoveAction = { type: typeof REMOVE, payload: string }
+type RentAction = { type: typeof RENT, payload: string }
+type ReturnAction = { type: typeof RETURN, payload: string }
+type Action = AddAction | RemoveAction | RentAction | ReturnAction;
 
 // REDUCER
-export const rentalOffice = (state: Product[] = initialState, action: any): any => {
+export const rentalOffice = (state: Product[] = initialState, action: Action): Product[] => {
     switch (action.type) {
+        case ADD:
+            return [...state, action.payload];
+        case REMOVE:
+            return state.filter(product => product.name !== action.payload);
+        case RENT:
+            return state.map(product => {
+                product.isAvailable = product.name === action.payload ? false : product.isAvailable
+                return product;
+            })
+        case RETURN:
+            return state.map(product => product.name === action.payload
+                ? ({ ...product, isAvailable: true }) : product)
         default:
             return state;
     }
 }
 
 // ACTION CREATOR
+export const createAddAction = (product: Product): AddAction => ({ type: ADD, payload: product });
+export const createRemoveAction = (productName: string): RemoveAction => ({ type: REMOVE, payload: productName });
+export const createRentAction = (productName: string): RentAction => ({ type: RENT, payload: productName });
+export const createReturnAction = (productName: string): ReturnAction => ({ type: RETURN, payload: productName });
 
