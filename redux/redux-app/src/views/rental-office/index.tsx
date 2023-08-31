f2036ec9b88c52c56dafb8ef633cdf677d568881
Kamil Richert <krichert@atlassian.com>
2023-08-31 13:17:01 +0200

    Finsihed rental office view

--- redux/redux-app/src/views/rental-office/index.tsx
+++ redux/redux-app/src/views/rental-office/index.tsx
@@ -1,6 +1,32 @@
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material"
+import { Button, Icon, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material"
+import { useSelector, useDispatch } from "react-redux"
+import { useForm } from "react-hook-form"
+import { State } from "../../store"
+import { createAddAction, createRemoveAction, createRentAction, createReturnAction } from "../../state/rental-office"
 
 export const RentalOffice = () => {
+    const { register, handleSubmit } = useForm({
+        defaultValues: { name: '' }
+    });
+    const products = useSelector((state: State) => state.rentalOffice);
+    const dispatch = useDispatch();
+
+    const onSubmit = ({ name }: { name: string }) => {
+        dispatch(createAddAction({ name, price: 10, isAvailable: true }))
+    }
+
+    const handleRemoveClick = (productName: string) => {
+        dispatch(createRemoveAction(productName))
+    }
+
+    const handleRentClick = (productName: string) => {
+        dispatch(createRentAction(productName))
+    }
+
+    const handleReturnClick = (productName: string) => {
+        dispatch(createReturnAction(productName))
+    }
+
     return (
         <>
             <Typography variant="h2">Rental office</Typography>
@@ -11,20 +37,38 @@ export const RentalOffice = () => {
                         <TableCell>Name</TableCell>
                         <TableCell align="right">Price</TableCell>
                         <TableCell align="right">Availability</TableCell>
+                        <TableCell sx={{ width: '100px'}} />
+                        <TableCell sx={{ width: '100px'}} />
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="right">Price</TableCell>
-                        <TableCell align="right">Availability</TableCell>
-                    </TableRow>
+                    {
+                        products.map((product) => (
+                            <TableRow key={product.name}>
+                                <TableCell>{product.name}</TableCell>
+                                <TableCell align="right">{product.price}</TableCell>
+                                <TableCell align="right">
+                                    {product.isAvailable 
+                                        ? <Icon>check_circle</Icon> 
+                                        : <Icon>block</Icon>}
+                                </TableCell>
+                                <TableCell>
+                                    {product.isAvailable 
+                                        ? <Button color="success" onClick={() => handleRentClick(product.name)}>Rent</Button>
+                                        : <Button color="warning" onClick={() => handleReturnClick(product.name)}>Return</Button>}
+                                </TableCell>
+                                <TableCell>
+                                    <Button color="error" onClick={() => handleRemoveClick(product.name)}>Remove</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
                 </TableBody>
                 </Table>
             </TableContainer>
-            <form style={{ marginTop: '15px'}}>
+            <form style={{ marginTop: '15px'}} onSubmit={handleSubmit(onSubmit)}>
                 <Typography variant="h6">Add new item</Typography>
-                <TextField label="New item" size="small" />
+                <TextField label="New item" size="small" {...register('name')} />
                 <Button type="submit">Add</Button>
             </form>
         </>
