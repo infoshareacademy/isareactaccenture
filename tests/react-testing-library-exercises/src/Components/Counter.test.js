da601b365fb24c113b47bb0204e2cdaa94eea2ed
Kamil Richert <krichert@atlassian.com>
2023-09-01 15:57:52 +0200

    Reaact testing library

--- tests/react-testing-library-exercises/src/Components/Counter.test.js
+++ tests/react-testing-library-exercises/src/Components/Counter.test.js
@@ -3,19 +3,37 @@ import Counter from './Counter';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('Counter', () => {
-  test('should have initial counter set to 0', () => {
+    test('should have initial counter set to 0', () => {
+        render(<Counter />);
 
-  });
+        expect(screen.getByText('value: 0')).toBeInTheDocument();
+    });
 
-  test('should have increase counter when increase clicked', () => {
+    test('should have increase counter when increase clicked', () => {
+        render(<Counter />);
 
-  });
+        fireEvent.click(screen.getByRole('button', { name: 'increase' }))
 
-  test('should have decrease counter when decrease clicked', () => {
+        expect(screen.getByText('value: 1')).toBeInTheDocument();
+    });
 
-  });
+    test('should have decrease counter when decrease clicked', () => {
+        render(<Counter />);
 
-  test('should have reset counter when reset clicked', () => {
+        fireEvent.click(screen.getByRole('button', { name: 'decrease' }))
 
-  });
+        expect(screen.getByText('value: -1')).toBeInTheDocument();
+    });
+
+    test('should have reset counter when reset clicked', () => {
+        render(<Counter />);
+
+        fireEvent.click(screen.getByRole('button', { name: 'increase' }))
+
+        expect(screen.getByText('value: 1')).toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole('button', { name: 'reset' }))
+
+        expect(screen.getByText('value: 0')).toBeInTheDocument();
+    });
 });
--- tests/react-testing-library-exercises/src/Components/TextTransformer.test.js
+++ tests/react-testing-library-exercises/src/Components/TextTransformer.test.js
@@ -5,19 +5,36 @@ import userEvent from '@testing-library/user-event';
 import TextTransformer from './TextTransformer';
 
 describe('TextTransformer', () => {
-  test('should transform inserted text', () => {
+    test('should transform inserted text', () => {
+        render(<TextTransformer />);
 
-  });
+        const input = screen.getByLabelText('Insert text');
+        userEvent.type(input, 'accenture')
 
-  test('should not override inserted text when prop has been set later', () => {
+        expect(screen.getByText('Transformed text:')).toBeInTheDocument();
+        expect(screen.getByText('__ACCENTURE__')).toBeInTheDocument();
+    });
 
-  });
+    test('should not override inserted text when prop has been set later', () => {
+        const { rerender } = render(<TextTransformer />);
 
-  test('should transform text from props when prop has been set', () => {
+        const input = screen.getByLabelText('Insert text');
+        userEvent.type(input, 'accenture')
 
-  });
+        rerender(<TextTransformer value='infoshare' />)
 
-  test('should not show transformed text when state is empty', () => {
+        expect(screen.getByText('__ACCENTURE__')).toBeInTheDocument();
+        expect(screen.queryByText('__INFOSHARE__')).not.toBeInTheDocument();
+    });
 
-  })
+    test('should transform text from props when prop has been set', () => {
+        render(<TextTransformer value='infoshare' />);
+        expect(screen.queryByText('__INFOSHARE__')).toBeInTheDocument();
+    });
+
+    test('should not show transformed text when state is empty', () => {
+        render(<TextTransformer />);
+
+        expect(screen.queryByText('Transformed text:')).not.toBeInTheDocument();
+    })
 });
