28f02aa482a026beab773fdfb9b52bc20ca6727e
Kamil Richert <krichert@atlassian.com>
2023-09-01 15:22:34 +0200

    Mock ex 1

--- tests/jest-mock-exercises/src/exercise-1/generator.test.js
+++ tests/jest-mock-exercises/src/exercise-1/generator.test.js
@@ -2,14 +2,26 @@
 import { getRandomNumber } from './math';
 import { generateId } from './generator'
 
+jest.mock('./math', () => ({
+    getRandomNumber: jest.fn()
+}))
+
 describe('generateId', () => {
     beforeEach(() => expect.hasAssertions())
 
     test('should generate id with given seed', () => {
+        getRandomNumber.mockReturnValue(50)
 
+        const result = generateId('accenture');
+
+        expect(result).toBe('id_accenture-50')
     });
 
     test('should generate id with given empty seed', () => {
+        getRandomNumber.mockReturnValue(50)
 
+        const result = generateId('');
+
+        expect(result).toBe('id_-50')
     });
 });
