859e6d6202f6c49e274b9e079bc234192c6073dc
Kamil Richert <krichert@atlassian.com>
2023-09-01 15:30:27 +0200

    Jest mock ex2

--- tests/jest-mock-exercises/src/exercise-2/todos-service.test.js
+++ tests/jest-mock-exercises/src/exercise-2/todos-service.test.js
@@ -3,14 +3,28 @@
 import { addTodo } from './todos-service';
 import { postTask } from './server';
 
+jest.mock('./server', () => ({
+    postTask: jest.fn()
+}))
+
 describe('Todo service', () => {
     test('should return success for valid task', () => {
+        const result = addTodo('zrobić zakupy');
 
+        expect(result).toEqual({
+            success: true
+        })
     });
     test('should post task for valid task', () => {
+        addTodo('zrobić zakupy');
 
+        expect(postTask).toHaveBeenCalledWith('zrobić zakupy');
     });
     test('should return error for invalid task', () => {
+        const result = addTodo('');
 
+        expect(result).toEqual({
+            error: 'Given task is not valid'
+        })
     });
 })
